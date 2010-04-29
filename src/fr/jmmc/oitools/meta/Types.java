c0b999455ddc7a0b78cebf2212e89a92e996f883
bourgesl <bourges.laurent@gmail.com>
2010-04-29 15:46:18 +0000

    added getDataType(Object)

--- src/fr/jmmc/oitools/meta/Types.java
+++ src/fr/jmmc/oitools/meta/Types.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: Types.java,v 1.1 2010-04-28 14:45:44 bourgesl Exp $"
+ * "@(#) $Id: Types.java,v 1.2 2010-04-29 15:46:18 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2010/04/28 14:45:44  bourgesl
+ * meta data package with Column and Keyword descriptors, Types and Units enumeration
+ *
  */
 package fr.jmmc.oitools.meta;
 
@@ -43,5 +46,34 @@ public enum Types {
 
   public char getRepresentation() {
     return representation;
+  }
+
+  /**
+   * Get the data type corresponding to the given value
+   * Does not support an array value
+   *
+   * @param value keyword value
+   *
+   * @return data type if it is known, null otherwise.
+   */
+  protected static Types getDataType(final Object value) {
+
+    if (value instanceof String) {
+      return Types.TYPE_CHAR;
+    }
+    if (value instanceof Double) {
+      return Types.TYPE_DBL;
+    }
+    if (value instanceof Integer) {
+      return Types.TYPE_INT;
+    }
+    if (value instanceof Float) {
+      return Types.TYPE_REAL;
+    }
+    if (value instanceof Boolean) {
+      return Types.TYPE_LOGICAL;
+    }
+
+    return null;
   }
 }
