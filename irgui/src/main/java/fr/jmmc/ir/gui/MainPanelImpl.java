47f72fd5f19e9fce488b9a5f9139232dd163b8ec
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2016-06-16 07:37:50 +0000

    will be retagged with the same version

--- irgui/src/main/java/fr/jmmc/ir/gui/MainPanelImpl.java
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
- ******************************************************************************/
-package fr.jmmc.ir.gui;
-
-
-public class MainPanelImpl extends MainPanel {
-
-}
--- oiexplorer-core/src/main/java/fr/jmmc/oiexplorer/core/model/plot/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.jmmc.fr/oiexplorer-core-plot-definition/0.1")
-package fr.jmmc.oiexplorer.core.model.plot;
