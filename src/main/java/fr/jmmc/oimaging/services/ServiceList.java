4fbb9a62c21e39e1b50fa2aca75ee675ce07e29d
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2016-11-18 13:02:00 +0000

    add a prefered Service

--- src/main/java/fr/jmmc/oimaging/services/ServiceList.java
+++ src/main/java/fr/jmmc/oimaging/services/ServiceList.java
@@ -17,6 +17,9 @@ public class ServiceList {
     /** Singleton instance */
     private static ServiceList _instance = null;
 
+    /** Default service */
+    private Service preferedService = null;
+
     GenericListModel<Service> availableServices = null;
 
     private ServiceList() {
@@ -25,17 +28,28 @@ public class ServiceList {
         OImagingExecutionMode localExecutionMode = new LocalExecutionMode();
 
         availableServices = new GenericListModel<Service>(new ArrayList<Service>(25), true);
-        availableServices.add(new Service("DummyOI", "dummy", dummyExecutionMode, "This is a dumy service", "by G.MELLA"));
+        availableServices.add(new Service("DummyOI", "dummy", dummyExecutionMode, "This is a dummy service", "by G.MELLA"));
         availableServices.add(new Service("BSMEM", "bsmem-ci", localExecutionMode, "", ""));
-        availableServices.add(new Service("BSMEM (remote)", "bsmem-ci", remoteExecutionMode, "", ""));
+
+        // TODO remove code configuration and link this it to a preference
+        preferedService = new Service("BSMEM (remote)", "bsmem-ci", remoteExecutionMode, "", "");
+        availableServices.add(preferedService);
 
     }
 
-    public static ComboBoxModel getAvailableServices() {
+    private static ServiceList getInstance() {
         if (_instance == null) {
             _instance = new ServiceList();
         }
-        return _instance.availableServices;
+        return _instance;
+    }
+
+    public static ComboBoxModel getAvailableServices() {
+        return getInstance().availableServices;
+    }
+
+    public static Service getPreferedService() {
+        return getInstance().preferedService;
     }
 
     public static ServiceResult reconstructsImage(Service service, File inputFile) {
