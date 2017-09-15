0696105a4e317bde4d43b1b0bd0eae4dd06520c2
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2017-09-15 15:35:49 +0000

    fix init that fixes parameter's salad

--- src/main/java/fr/jmmc/oimaging/model/IRModel.java
+++ src/main/java/fr/jmmc/oimaging/model/IRModel.java
@@ -148,6 +148,8 @@ public class IRModel {
         // avoid null service
         if (getSelectedService() == null) {
             setSelectedSoftware(ServiceList.getPreferedService());
+        } else {
+            initSpecificParams();
         }
     }
 
