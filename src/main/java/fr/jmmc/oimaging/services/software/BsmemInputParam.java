724f33b04a2e391e659db5fb62fd18e7079d9d05
bourgesl <bourges.laurent@gmail.com>
2018-10-10 12:16:00 +0000

    added FLUXERR (not THRESHOLD) for WISARD

--- src/main/java/fr/jmmc/oimaging/services/software/BsmemInputParam.java
+++ src/main/java/fr/jmmc/oimaging/services/software/BsmemInputParam.java
@@ -5,7 +5,6 @@ package fr.jmmc.oimaging.services.software;
 
 import fr.jmmc.oitools.image.ImageOiConstants;
 import fr.jmmc.oitools.image.ImageOiInputParam;
-import static fr.jmmc.oitools.meta.CellMeta.NO_STR_VALUES;
 import fr.jmmc.oitools.meta.KeywordMeta;
 import fr.jmmc.oitools.meta.Types;
 import java.util.List;
@@ -15,14 +14,14 @@ import java.util.List;
  */
 public final class BsmemInputParam extends SoftwareInputParam {
 
-    public final static String AUTO_WGT = "AUTO_WGT";
-    public final static String FLUXERR = "FLUXERR";
+    public final static String KEYWORD_AUTO_WGT = "AUTO_WGT";
+    public final static String KEYWORD_FLUXERR = "FLUXERR";
 
     // Bsmem specific
-    private final static KeywordMeta KEYWORD_AUTO_WGT = new KeywordMeta(AUTO_WGT,
-            "Automatic regularization weight", Types.TYPE_LOGICAL, true, NO_STR_VALUES);
-    private final static KeywordMeta KEYWORD_FLUXERR = new KeywordMeta(FLUXERR,
-            "Error on zero-baseline V^2 point", Types.TYPE_DBL, true, NO_STR_VALUES);
+    private final static KeywordMeta AUTO_WGT = new KeywordMeta(KEYWORD_AUTO_WGT,
+            "Automatic regularization weight", Types.TYPE_LOGICAL);
+    private final static KeywordMeta FLUXERR = new KeywordMeta(KEYWORD_FLUXERR,
+            "Error on zero-baseline V^2 point", Types.TYPE_DBL); // like wisard
 
     // Potential Conflict with ImageOiInputParam.KEYWORD_RGL_NAME ?
     public static final String[] RGL_NAME_BSMEM = new String[]{"mem_prior"};
@@ -36,14 +35,14 @@ public final class BsmemInputParam extends SoftwareInputParam {
         super.update(params);
 
         // define keywords:
-        params.addKeyword(KEYWORD_AUTO_WGT);
-        params.addKeyword(KEYWORD_FLUXERR);
+        params.addKeyword(AUTO_WGT);
+        params.addKeyword(FLUXERR);
 
         // default values:
-        params.setKeywordDefaultLogical(AUTO_WGT, true);
-        params.setKeywordDefaultDouble(FLUXERR, 0.1);
+        params.setKeywordDefaultLogical(KEYWORD_AUTO_WGT, true);
+        params.setKeywordDefaultDouble(KEYWORD_FLUXERR, 0.1);
 
-        if (params.getKeywordLogical(AUTO_WGT)) {
+        if (params.getKeywordLogical(KEYWORD_AUTO_WGT)) {
             params.removeKeyword(ImageOiConstants.KEYWORD_RGL_WGT);
         }
     }
@@ -51,7 +50,7 @@ public final class BsmemInputParam extends SoftwareInputParam {
     @Override
     public void validate(final ImageOiInputParam params, final List<String> failures) {
         // custom validation rules:
-        final double fluxerr = params.getKeywordDouble(FLUXERR);
+        final double fluxerr = params.getKeywordDouble(KEYWORD_FLUXERR);
 
         if (fluxerr < 1e-5) {
             failures.add("FluxErr must be greater than 1e-5");
--- src/main/java/fr/jmmc/oimaging/services/software/WisardInputParam.java
+++ src/main/java/fr/jmmc/oimaging/services/software/WisardInputParam.java
@@ -23,16 +23,23 @@ public final class WisardInputParam extends SoftwareInputParam {
 
     public static final String KEYWORD_FOV = "FOV";
     public static final String KEYWORD_NP_MIN = "NP_MIN";
-    public static final String KEYWORD_THRESHOLD = "THRESHOL";
+    public final static String KEYWORD_FLUXERR = "FLUXERR";
+    // optional
     public static final String KEYWORD_SCALE = "SCALE";
     public static final String KEYWORD_DELTA = "DELTA";
 
-    private static final KeywordMeta FOV = new KeywordMeta(KEYWORD_FOV, "field of view", Types.TYPE_DBL);
-    private static final KeywordMeta NP_MIN = new KeywordMeta(KEYWORD_NP_MIN, "minimum number of reconstructed voxels", Types.TYPE_INT);
-    private static final KeywordMeta THRESHOLD = new KeywordMeta(KEYWORD_THRESHOLD, "threshold value to stop iterations", Types.TYPE_DBL);
+    // Wisard specific
+    private static final KeywordMeta FOV = new KeywordMeta(KEYWORD_FOV,
+            "Field of view", Types.TYPE_DBL);
+    private static final KeywordMeta NP_MIN = new KeywordMeta(KEYWORD_NP_MIN,
+            "Minimum number of reconstructed voxels", Types.TYPE_INT);
+    private final static KeywordMeta FLUXERR = new KeywordMeta(KEYWORD_FLUXERR,
+            "Error on ... TBD", Types.TYPE_DBL); // like bsmem
     // optional
-    private static final KeywordMeta SCALE = new KeywordMeta(KEYWORD_SCALE, "TBD", Types.TYPE_DBL);
-    private static final KeywordMeta DELTA = new KeywordMeta(KEYWORD_DELTA, "TBD", Types.TYPE_DBL);
+    private static final KeywordMeta SCALE = new KeywordMeta(KEYWORD_SCALE,
+            "TBD", Types.TYPE_DBL);
+    private static final KeywordMeta DELTA = new KeywordMeta(KEYWORD_DELTA,
+            "TBD", Types.TYPE_DBL);
 
     // TODO: 
     // - L1L2WHITE/PSD: add MEAN_OBJECT KW: An image, rescaled NP_MINxNP_MIN as INIT_IMG. (NULL) 
@@ -56,12 +63,12 @@ public final class WisardInputParam extends SoftwareInputParam {
         // define keywords:
         params.addKeyword(FOV);
         params.addKeyword(NP_MIN);
-        params.addKeyword(THRESHOLD);
+        params.addKeyword(FLUXERR);
 
         // default values:
         params.setKeywordDefaultDouble(KEYWORD_FOV, 20.0);
         params.setKeywordDefaultInt(KEYWORD_NP_MIN, 32);
-        params.setKeywordDefaultDouble(KEYWORD_THRESHOLD, 1e-6);
+        params.setKeywordDefaultDouble(KEYWORD_FLUXERR, 0.01);
 
         // for our first implementation, just add to params if not TOTVAR
         if (params.getRglName().startsWith(RGL_NAME_WISARD_L1)) {
