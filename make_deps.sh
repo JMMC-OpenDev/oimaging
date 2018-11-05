0c7b537d7e597310d0ea95b730e8bf281fb02e36
bourgesl <bourges.laurent@gmail.com>
2018-11-05 13:44:24 +0000

    fixed script

--- make_deps.sh
+++ make_deps.sh
@@ -16,42 +16,44 @@ cd jmcs
     cd ..
     mvn process-resources
 
-mvn $MVN_OPTS clean install 
+mvn $MVN_OPTS clean install
 cd ..
 
     # 1.2. testgui:
     # first time only ?
     svn export https://svn.jmmc.fr/jmmc-sw/MCS/trunk/testgui
     cd testgui
-    mvn $MVN_OPTS clean install 
+    mvn $MVN_OPTS clean install
     cd ..
 
 
 # 2. jmal:
 svn export https://svn.jmmc.fr/jmmc-sw/MCS/trunk/jmal
 cd jmal
-mvn $MVN_OPTS clean install 
+mvn process-resources
+mvn $MVN_OPTS clean install
 cd ..
 
 
 # 3. oitools:
 git clone https://github.com/JMMC-OpenDev/OITools.git
 cd OITools
-mvn $MVN_OPTS clean install 
+mvn process-resources
+mvn $MVN_OPTS clean install
 cd ..
 
 
 # 4. oiexplorer-core:
 svn export https://svn.jmmc.fr/jmmc-sw/oiTools/trunk/oiexplorer-core
 cd oiexplorer-core
-mvn $MVN_OPTS clean install 
+mvn $MVN_OPTS clean install
 cd ..
 
 
 # 5. oimaging:
 # svn export https://svn.jmmc.fr/jmmc-sw/oiTools/trunk/oimaging
 # cd oimaging
-# mvn $MVN_OPTS clean install 
+# mvn $MVN_OPTS clean install
 # cd ..
 
 echo "build done"
