c56a9daef71d3388ea4a854ed7dee6f14ecf64a8
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2019-06-05 05:36:08 +0000

    Automatically tagged OiDB-deps in version OIDB_V1_3-alpha from trunk  (admManager.sh)

--- oiexplorer/fitsTableViewer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# CLI FitsTableViewer
-#
-# list:
-#   ./oifitsTableViewer.sh /home/bourgesl/oidata/2007-06-29.fits
-#
-
-java -cp ./target/oiexplorer-TRUNK-jar-with-dependencies.jar fr.jmmc.oiexplorer.core.gui.FitsTableViewer $1
