0ea3ddaab90561ccc69e616c4bd321045055007e
bourgesl <bourges.laurent@gmail.com>
2019-10-24 13:26:40 +0000

    added sparco service

--- src/main/java/fr/jmmc/oimaging/services/ServiceList.java
+++ src/main/java/fr/jmmc/oimaging/services/ServiceList.java
@@ -5,7 +5,6 @@ package fr.jmmc.oimaging.services;
 
 import fr.jmmc.jmcs.gui.component.GenericListModel;
 import fr.jmmc.oimaging.services.software.SoftwareInputParam;
-import java.io.File;
 import java.util.ArrayList;
 import javax.swing.ComboBoxModel;
 
@@ -15,12 +14,16 @@ import javax.swing.ComboBoxModel;
  */
 public final class ServiceList {
 
+    private static final boolean ENABLE_LOCAL_MODE = true;
+
     public static final String SERVICE_BSMEM = "BSMEM";
     public static final String SERVICE_MIRA = "MIRA";
+    public static final String SERVICE_SPARCO = "SPARCO";
     public static final String SERVICE_WISARD = "WISARD";
 
     public static final String CMD_BSMEM = "bsmem-ci";
     public static final String CMD_MIRA = "mira-ci";
+    public static final String CMD_SPARCO = "sparco-ci";
     public static final String CMD_WISARD = "wisard-ci";
 
     /** Singleton instance */
@@ -33,24 +36,28 @@ public final class ServiceList {
 
     private ServiceList() {
         final OImagingExecutionMode remoteExecutionMode = RemoteExecutionMode.INSTANCE;
-        final OImagingExecutionMode localExecutionMode = LocalExecutionMode.INSTANCE;
 
         availableServices = new GenericListModel<Service>(new ArrayList<Service>(8), true);
 
         final SoftwareInputParam swParamBsmem = SoftwareInputParam.newInstance(SERVICE_BSMEM);
         final SoftwareInputParam swParamMira = SoftwareInputParam.newInstance(SERVICE_MIRA);
+        final SoftwareInputParam swParamSparco = SoftwareInputParam.newInstance(SERVICE_SPARCO);
         final SoftwareInputParam swParamWisard = SoftwareInputParam.newInstance(SERVICE_WISARD);
-
-        availableServices.add(new Service(SERVICE_BSMEM + " (local)", CMD_BSMEM, localExecutionMode, "", "", swParamBsmem));
-        availableServices.add(new Service(SERVICE_MIRA + " (local)", CMD_MIRA, localExecutionMode, "", "", swParamMira));
-        availableServices.add(new Service(SERVICE_WISARD + " (local)", CMD_WISARD, localExecutionMode, "", "", swParamWisard));
 
         availableServices.add(new Service(SERVICE_BSMEM, CMD_BSMEM, remoteExecutionMode, "", "", swParamBsmem));
         availableServices.add(new Service(SERVICE_MIRA, CMD_MIRA, remoteExecutionMode, "", "", swParamMira));
-        // TODO remove code configuration and link this it to a preference
+        availableServices.add(new Service(SERVICE_SPARCO, CMD_SPARCO, remoteExecutionMode, "", "", swParamSparco));
+        // TODO define prefered service in a preference:
         preferedService = new Service(SERVICE_WISARD, CMD_WISARD, remoteExecutionMode, "", "", swParamWisard);
-
         availableServices.add(preferedService);
+        
+        if (ENABLE_LOCAL_MODE) {
+            final OImagingExecutionMode localExecutionMode = LocalExecutionMode.INSTANCE;
+            availableServices.add(new Service(SERVICE_BSMEM + " (local)", CMD_BSMEM, localExecutionMode, "", "", swParamBsmem));
+            availableServices.add(new Service(SERVICE_MIRA + " (local)", CMD_MIRA, localExecutionMode, "", "", swParamMira));
+            availableServices.add(new Service(SERVICE_SPARCO + " (local)", CMD_SPARCO, localExecutionMode, "", "", swParamSparco));
+            availableServices.add(new Service(SERVICE_WISARD + " (local)", CMD_WISARD, localExecutionMode, "", "", swParamWisard));
+        }
     }
 
     private static ServiceList getInstance() {
@@ -66,8 +73,8 @@ public final class ServiceList {
 
     public static Service getAvailableService(final String name) {
         final ComboBoxModel model = getInstance().availableServices;
-        for (int i = 0, len =  model.getSize(); i < len; i++) {
-            final Service service = (Service)model.getElementAt(i);
+        for (int i = 0, len = model.getSize(); i < len; i++) {
+            final Service service = (Service) model.getElementAt(i);
             if (service.getName().equalsIgnoreCase(name)) {
                 return service;
             }
