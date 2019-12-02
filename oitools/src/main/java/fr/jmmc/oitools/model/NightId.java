e90d716c600702fb1420db4a7f48f655792cc4ed
bourgesl <bourges.laurent@gmail.com>
2019-12-02 13:32:42 +0000

    Automatically tagged OImaging in version OImaging_V0_5_2 from trunk  (admManager.sh)

---
+++ oitools/src/main/java/fr/jmmc/oitools/model/NightId.java
@@ -0,0 +1,120 @@
+/* 
+ * Copyright (C) 2018 CNRS - JMMC project ( http://www.jmmc.fr )
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.oitools.model;
+
+import fr.jmmc.jmcs.util.NumberUtils;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A value-type (immutable) representing a single night in any OI_Data table
+ * @author bourgesl
+ */
+public final class NightId {
+
+    public final static NightId UNDEFINED = new NightId();
+
+    public static final Comparator<NightId> CMP_NIGHT = new Comparator<NightId>() {
+        @Override
+        public int compare(NightId n1, NightId n2) {
+            return NumberUtils.compare(n1.getNightId(), n2.getNightId());
+        }
+    };
+
+    /** cached NightId instances */
+    private final static Map<NightId, NightId> CACHE = new HashMap<NightId, NightId>(64);
+
+    public static NightId getCachedInstance(final int nightId) {
+        NightId n = new NightId(nightId);
+        NightId night = getCachedInstance(n);
+        if (night == null) {
+            night = putCachedInstance(n);
+        }
+        return night;
+    }
+    
+    public static NightId getCachedInstance(final NightId nightId) {
+        return CACHE.get(nightId);
+    }
+
+    public static NightId putCachedInstance(final NightId nightId) {
+        CACHE.put(nightId, nightId);
+        return nightId;
+    }
+
+    /* members */
+    /** nightId = rounded MJD */
+    private int nightId;
+
+    public NightId() {
+        this(Integer.MAX_VALUE);
+    }
+
+    private NightId(final int nightId) {
+        set(nightId);
+    }
+
+    /**
+     * Warning: never alter a cached instance !
+     * @param nightId nightId value
+     */
+    public void set(final int nightId) {
+        this.nightId = nightId;
+    }
+
+    public int getNightId() {
+        return nightId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.nightId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final NightId other = (NightId) obj;
+        if (this.nightId != other.getNightId()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "NightId{" + nightId + '}';
+    }
+
+}
---
+++ oitools/src/test/java/fr/jmmc/oitools/util/test/TestProcessorCommandLine.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2018 CNRS - JMMC project ( http://www.jmmc.fr )
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.jmmc.oitools.util.test;
+
+import fr.jmmc.oitools.JUnitBaseTest;
+import static fr.jmmc.oitools.JUnitBaseTest.TEST_DIR_OIFITS;
+import fr.jmmc.oitools.OIFitsProcessor;
+import fr.jmmc.oitools.model.OIFitsFile;
+import fr.jmmc.oitools.model.OIFitsLoader;
+import fr.nom.tam.fits.FitsException;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author jammetv
+ */
+public class TestProcessorCommandLine {
+
+    private static final int OUT_INDEX = 0;
+    private static final int ERR_INDEX = 1;
+
+    @Test
+    public void testNoInputFile() {
+        String[] result = callProcessor(new String[]{"merge"});
+        Assert.assertTrue("Bad return message: " + result[ERR_INDEX],
+                getError(result).startsWith("No file location given in arguments."));
+    }
+
+    @Test
+    public void testNoOutputFile() {
+        String[] result = callProcessor(new String[]{"merge",
+            TEST_DIR_OIFITS + "A-CLUSTER__2T3T__1-PHASEREF__SIMPLE_nsr0.05__20160812_193521_1.image-oi.oifits",
+            "-o"});
+        Assert.assertTrue("Bad return message: " + result[ERR_INDEX],
+                getError(result).startsWith("No output file"));
+    }
+
+    @Test
+    public void testOk() {
+        File output = new File(JUnitBaseTest.TEST_DIR_TEST + "merge_result.fits");
+        output.delete();
+        String[] result = callProcessor(new String[]{"merge", "-o", output.getAbsolutePath(),
+            TEST_DIR_OIFITS + "A-CLUSTER__2T3T__1-PHASEREF__SIMPLE_nsr0.05__20160812_193521_1.image-oi.oifits",
+            TEST_DIR_OIFITS + "A-CLUSTER__2T3T__1-PHASEREF__SIMPLE_nsr0.05__20160812_193521_1.oifits"
+        });
+        Assert.assertTrue("Bad return message: " + result[OUT_INDEX],
+                getOut(result).startsWith("Writing"));
+        Assert.assertTrue("No result file created", output.exists());
+    }
+
+    @Test
+    public void testList() {
+        String[] result = callProcessor(new String[]{"list",
+            TEST_DIR_OIFITS + "A-CLUSTER__2T3T__1-PHASEREF__SIMPLE_nsr0.05__20160812_193521_1.image-oi.oifits"
+        });
+        Assert.assertTrue("Bad return message: " + result[OUT_INDEX],
+                getOut(result).startsWith("target_name"));
+    }
+
+    @Test
+    public void testConvert() {
+        File output = new File(JUnitBaseTest.TEST_DIR_TEST + "convert_result.fits");
+        output.delete();
+        
+        callProcessor(new String[]{"convert", "-o", output.getAbsolutePath(),
+            TEST_DIR_OIFITS + "A-CLUSTER__2T3T__1-PHASEREF__SIMPLE_nsr0.05__20160812_193521_1.image-oi.oifits"
+        });
+        Assert.assertTrue("No output file present.", output.exists());
+    }
+
+    @Test
+    public void testMergeFilterInsname() throws IOException, MalformedURLException, FitsException {
+        File output = new File(JUnitBaseTest.TEST_DIR_TEST + "merge_filter_result.fits");
+        output.delete();
+
+        // Filter block
+        String[] result = callProcessor(new String[]{"merge",
+            "-o", output.getAbsolutePath(), "-insname", "SPECTRO_SC",
+            TEST_DIR_OIFITS + "NGC5128_2005.oifits"
+        });
+        Assert.assertTrue("Bad return message: " + result[OUT_INDEX], 
+                getOut(result).startsWith("Result is empty, no file created."));
+        Assert.assertTrue("No result file should be created", !output.exists());
+
+        // Filter pass
+        result = callProcessor(new String[]{"merge",
+            "-o", output.getAbsolutePath(), "-insname", "MIDI/PRISM",
+            TEST_DIR_OIFITS + "NGC5128_2005.oifits"
+        });
+        Assert.assertTrue("Bad return message: " + result[OUT_INDEX], 
+                getOut(result).startsWith("Writing"));
+        OIFitsFile mergedFile = OIFitsLoader.loadOIFits(output.getAbsolutePath());
+        Assert.assertEquals("Bad number of Ins in merged file", 1, mergedFile.getOiWavelengths().length);
+    }
+
+    private static String getOut(String[] result) {
+        return result[OUT_INDEX] != null ? result[OUT_INDEX] : "";
+    }
+
+    private static String getError(String[] result) {
+        return result[ERR_INDEX] != null ? result[ERR_INDEX] : "";
+    }
+
+    /**
+     * Call merge, return out and err stream of the operation
+     *
+     * @param args
+     * @return String[2], [0]: out, [1]: err
+     */
+    private static String[] callProcessor(String[] args) {
+        String[] result = new String[2];
+
+        // Create a stream to hold the output
+        PrintStream psOut = null;
+        PrintStream psErr = null;
+
+        try {
+
+            ByteArrayOutputStream baosOut = new ByteArrayOutputStream();
+            ByteArrayOutputStream baosErr = new ByteArrayOutputStream();
+            psOut = new PrintStream(baosOut);
+            psErr = new PrintStream(baosErr);
+            // IMPORTANT: Save the old System.out!
+            PrintStream oldOut = System.out;
+            PrintStream oldErr = System.err;
+            // Tell Java to use special stream
+            System.setOut(psOut);
+            System.setErr(psErr);
+
+            // Call operation
+            OIFitsProcessor.main(args);
+
+            System.out.flush();
+            System.err.flush();
+            System.setOut(oldOut);
+            System.setErr(oldErr);
+
+            result[0] = baosOut.toString();
+            result[1] = baosErr.toString();
+            return result;
+
+        } finally {
+            if (psOut != null) {
+                psOut.close();
+            }
+            if (psErr != null) {
+                psErr.close();
+            }
+        }
+    }
+
+}
