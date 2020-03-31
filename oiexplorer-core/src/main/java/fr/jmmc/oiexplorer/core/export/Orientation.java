4873a19c5bd66f6aae6a68a4c697f0a743ae8280
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2020-03-31 15:43:48 +0000

    Automatically tagged OiDB-deps in version OIDB_V1_7-beta from trunk  (admManager.sh)

---
+++ oiexplorer-core/src/main/java/fr/jmmc/oiexplorer/core/export/Orientation.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.oiexplorer.core.export;
+
+/** Page orientation enumeration */
+public enum Orientation {
+
+    Portrait, Landscape
+
+}
