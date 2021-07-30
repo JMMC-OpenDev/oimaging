159ce028cf8fcc7750e78a6b72e525c8d146e1eb
Martin Pratoussy <martin.pratoussy@cpe.fr>
2021-07-30 09:39:52 +0200

    Replace colorized background with green check mark and red x mark

--- src/main/java/fr/jmmc/oimaging/gui/SuccessCell.java
+++ src/main/java/fr/jmmc/oimaging/gui/SuccessCell.java
@@ -5,26 +5,54 @@
 
 package fr.jmmc.oimaging.gui;
 
-import java.awt.Color;
-import java.awt.Component;
-import javax.swing.JLabel;
-import javax.swing.JTable;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 
 /**
  *
  * @author martin
  */
-public class SuccessCell extends JLabel implements TableCellRenderer {
+public class SuccessCell extends JPanel implements TableCellRenderer {
+
+    private BufferedImage greenCheckMark;
+    private BufferedImage redXMark;
+    private boolean success = false;
 
     public SuccessCell() {
-        super.setOpaque(true);
+
+        try {
+            /**
+             * License: Creative Commons Attribution-NoDerivs 3.0
+             * https://www.iconsdb.com/green-icons/ok-icon.html
+             */
+            greenCheckMark = ImageIO.read(new File("src/main/resources/fr/jmmc/oimaging/resource/image/ok-16.png"));
+            /**
+             * License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+             * https://www.iconsdb.com/green-icons/x-mark-3-icon.html
+             */
+            redXMark = ImageIO.read(new File("src/main/resources/fr/jmmc/oimaging/resource/image/x-mark-3-16.png"));
+        } catch (IOException e) {
+            System.out.println("Can't load images");
+        }
+
     }
     
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        super.setBackground((boolean) value ? Color.GREEN : Color.RED);
+        success = (boolean) value;
         return this;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage((success) ? greenCheckMark : redXMark, this.getWidth()/2, 0, this);
     }
     
 }
