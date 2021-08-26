6cac3c345b7ef41beff02692176b07affabe6c02
bourgesl <bourges.laurent@gmail.com>
2021-08-26 21:45:21 +0200

    refactoring to use explicit load... methods instead of lazy getOIfits / getExecutionLog that can throw exceptions

--- src/main/java/fr/jmmc/oimaging/services/ServiceResult.java
+++ src/main/java/fr/jmmc/oimaging/services/ServiceResult.java
@@ -3,9 +3,7 @@
  ******************************************************************************/
 package fr.jmmc.oimaging.services;
 
-import fr.jmmc.jmcs.gui.FeedbackReport;
 import fr.jmmc.jmcs.util.FileUtils;
-import fr.jmmc.oimaging.gui.StarRater;
 import fr.jmmc.oitools.model.OIFitsFile;
 import fr.jmmc.oitools.model.OIFitsLoader;
 import fr.nom.tam.fits.FitsException;
@@ -18,7 +16,7 @@ import java.util.Date;
  * @author mellag
  */
 public final class ServiceResult {
-
+    
     private final File inputFile;
     private final File oifitsResultFile;
     private final File executionLogResultFile;
@@ -32,7 +30,7 @@ public final class ServiceResult {
     // User appreciation attribute
     private int rating;
     private String comments;
-
+    
     private Service service;
 
     // Post process cached data
@@ -50,110 +48,116 @@ public final class ServiceResult {
         this.inputFile = inputFile;
         this.oifitsResultFile = FileUtils.getTempFile(inputFilename + ".output.fits");
         this.executionLogResultFile = FileUtils.getTempFile(inputFilename + ".log.txt");
-
+        
         this.rating = 0;
         this.comments = "No comments";
-
+        
         init();
     }
-
+    
     private void init() {
         setStartTime(new Date());
     }
-
+    
     public File getInputFile() {
         return inputFile;
     }
-
+    
     public File getOifitsResultFile() {
         return oifitsResultFile;
     }
-
-    public OIFitsFile getOifitsFile() throws IOException, FitsException {
-        if (oiFitsFile == null) {
-            oiFitsFile = OIFitsLoader.loadOIFits(oifitsResultFile.getAbsolutePath());
+    
+    public void loadOIFitsFile() throws IOException, FitsException {
+        if ((oiFitsFile == null) && (oifitsResultFile != null) && oifitsResultFile.exists()) {
+            try {
+                oiFitsFile = OIFitsLoader.loadOIFits(oifitsResultFile.getAbsolutePath());
+            } catch (IOException | FitsException e) {
+                setValid(false);
+                throw e;
+            }
         }
+    }
+    
+    public OIFitsFile getOifitsFile() {
         return oiFitsFile;
     }
-
+    
     public File getExecutionLogResultFile() {
         return executionLogResultFile;
     }
-
-    public String getExecutionLog() {
-        if (executionLog == null) {
-            try {
-                executionLog = FileUtils.readFile(executionLogResultFile.getAbsoluteFile());
-            } catch (IOException ex) {
-                FeedbackReport.openDialog(ex);
-                executionLog = "Can't recover log report.";
-            }
+    
+    public void loadExecutionLogFile() throws IOException {
+        if ((executionLog == null) && (executionLogResultFile != null) && executionLogResultFile.exists()) {
+            executionLog = FileUtils.readFile(executionLogResultFile.getAbsoluteFile());
         }
+    }
+    
+    public String getExecutionLog() {
         return executionLog;
     }
-
-    public int getStarRater() {
+    
+    public int getRating() {
         return rating;
     }
-
+    
     public String getComments() {
         return comments;
     }
-
+    
     public boolean isCancelled() {
         return cancelled;
     }
-
+    
     public void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
     }
-
+    
     public boolean isValid() {
         return valid;
     }
-
+    
     public void setValid(boolean valid) {
         this.valid = valid;
     }
-
+    
     public String getErrorMessage() {
         return errorMessage;
     }
-
+    
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
     }
-
+    
     public Date getStartTime() {
         return this.startTime;
     }
-
+    
     public Date getEndTime() {
         return this.endTime;
     }
-
+    
     public void setStartTime(Date startTime) {
         this.startTime = startTime;
     }
-
+    
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
-
+    
     public Service getService() {
         return service;
     }
-
+    
     public void setService(Service service) {
         this.service = service;
     }
-
+    
     public void setRating(int rating) {
         this.rating = rating;
     }
-
+    
     public void setComments(String comments) {
         this.comments = comments;
     }
-
+    
 }
