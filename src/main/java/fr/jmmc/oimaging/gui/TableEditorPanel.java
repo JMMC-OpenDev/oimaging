f01505045e671567ef2d9bf8c3456da4aa2fab5d
buthanoid <portail@buthanoid.eu>
2021-09-15 16:24:46 +0200

    JMMC header in TableEditorPanel

--- src/main/java/fr/jmmc/oimaging/gui/TableEditorPanel.java
+++ src/main/java/fr/jmmc/oimaging/gui/TableEditorPanel.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** *****************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ***************************************************************************** */
 package fr.jmmc.oimaging.gui;
 
 import fr.jmmc.oimaging.model.ResultSetTableModel;
