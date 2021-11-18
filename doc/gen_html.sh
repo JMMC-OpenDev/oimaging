b5b5506ef0652dc0ba46cc10ff77aa0bd5817eaa
bourgesl <bourges.laurent@gmail.com>
2021-11-18 15:27:24 +0100

    only run gen_html if dev profile

--- doc/gen_html.sh
+++ doc/gen_html.sh
@@ -3,13 +3,16 @@
 if type -p pandoc
 then
    for f in *.md; do 
-           
-       echo '######################'
-       echo "Processing file : $f "
-
        NAME=`basename $f .md`
-       pandoc --standalone --metadata pagetitle="${NAME}" ${NAME}.md > ${NAME}.html
+
+       if [ "${NAME}.md" -nt "${NAME}.html" ]; then
+           echo '######################'
+           echo "Processing file : $f "
+
+           pandoc --standalone --metadata pagetitle="${NAME}" ${NAME}.md > ${NAME}.html
+       fi
    done
 else
-	echo "pandoc command was not found. Did not parse any markdown documentation file."
+    echo "pandoc command was not found. Did not parse any markdown documentation file."
 fi
+
